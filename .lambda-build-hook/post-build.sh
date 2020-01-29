895ff42f9f63ed28bc7997cde5093d1a0515e5b2
Filip Sobalski <pinkeen@gmail.com>
2020-01-29 16:59:29 +0100

    Add libjpeg.so binaries to the deploy package

---
+++ .lambda-build-hook/post-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# ------------------- BINARY SYSTEM LIBRARIES ------------------
+# Copy libjpeg which is missing in edge lambda runtime even
+# though it should be present in base Amazon Linux 2 filesystem.
+#
+# The library libjpeg-turbo is required for cwebp-bin package
+# and I found no way to provide it purely through npm /FS
+yum -y install libjpeg-turbo
+mkdir -pv "${LAMBDA_SHARED_LIB_DIR}"
+cp -v /usr/lib64/libjpeg.so.* "${LAMBDA_SHARED_LIB_DIR}"
